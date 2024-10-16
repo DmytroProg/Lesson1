b5882a29f52ff3101828d0cd9d14e064db2d0b1c
DmytroProg <dmytroprog@gmail.com>
2024-10-16 18:25:11 +0300

    Add project files.

---
+++ Lesson1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Lesson1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1.Controllers;
+
+public class StudentController : Controller
+{
+    private Student _student = new Student
+    {
+        DateOfBirth = DateTime.Now,
+        FirstName = "Test",
+        LastName = "Test",
+        GPA = 5
+    };
+
+    public IActionResult GetStudents()
+    {
+        //ViewBag.Student = _student;
+        //ViewData["Student"] = _student;
+        return View(_student);
+    }
+}
---
+++ Lesson1/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson1.Models;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public double GPA { get; set; }
+}
